851d807ba13d158e72460ebc6a500928aa721bb9
KotrotsKon <kotrotskon@users.noreply.github.com>
2020-04-15 19:11:00 +0300

    add five buttons

--- app/src/main/java/com/example/android_addtwonums_example/MainActivity.java
+++ app/src/main/java/com/example/android_addtwonums_example/MainActivity.java
@@ -35,5 +35,66 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button btn_min = findViewById(R.id.btn_min);
+        btn_min.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strNum1 = edTxt_num1.getText().toString();
+                String strNum2 = edTxt_num2.getText().toString();
+
+                double num1 = Double.valueOf(strNum1);
+                double num2 = Double.valueOf(strNum2);
+
+                double sum = num1 - num2;
+                String strSum = String.valueOf(sum);
+
+                edTxt_result.setText(strSum);
+            }
+        });
+
+        Button btn_mul = findViewById(R.id.btn_mul);
+        btn_mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strNum1 = edTxt_num1.getText().toString();
+                String strNum2 = edTxt_num2.getText().toString();
+
+                double num1 = Double.valueOf(strNum1);
+                double num2 = Double.valueOf(strNum2);
+
+                double sum = num1 * num2;
+                String strSum = String.valueOf(sum);
+
+                edTxt_result.setText(strSum);
+            }
+        });
+
+        Button btn_div = findViewById(R.id.btn_div);
+        btn_div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strNum1 = edTxt_num1.getText().toString();
+                String strNum2 = edTxt_num2.getText().toString();
+
+                double num1 = Double.valueOf(strNum1);
+                double num2 = Double.valueOf(strNum2);
+
+                double sum = num1 / num2;
+                String strSum = String.valueOf(sum);
+
+                edTxt_result.setText(strSum);
+            }
+        });
+
+        Button btn_clr = findViewById(R.id.btn_clr);
+        btn_clr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edTxt_num1.setText("");
+                edTxt_num2.setText("");
+                edTxt_result.setText("");
+            }
+        });
+
     }
 }
